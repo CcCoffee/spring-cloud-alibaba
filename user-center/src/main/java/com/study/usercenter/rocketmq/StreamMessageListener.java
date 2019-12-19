c015ae33486ab6ddd34006ecfc3eb37bfdfd0563
Kevin K H Zheng <172853425@qq.com>
2019-12-19 20:59:32 +0800

    实现spring cloud stream全局异常处理

--- user-center/src/main/java/com/study/usercenter/rocketmq/StreamMessageListener.java
+++ user-center/src/main/java/com/study/usercenter/rocketmq/StreamMessageListener.java
@@ -3,6 +3,8 @@ package com.study.usercenter.rocketmq;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.ErrorMessage;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,5 +19,12 @@ public class StreamMessageListener {
     @StreamListener(MySink.MY_INPUT)
     public void receiveMyMessage(String messageBody){
         log.info("my message body : {}", messageBody);
+//        throw new IllegalArgumentException("接收消息发生异常");
+    }
+
+    @StreamListener("errorChannel")
+    public void error(Message<?> message){
+        ErrorMessage errorMessage = (ErrorMessage) message;
+        log.warn("发生异常：errorMessage = {}",errorMessage);
     }
 }
